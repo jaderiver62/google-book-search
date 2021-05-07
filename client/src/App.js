5db0b90198400658eee057233fe1564f79759a09
Nina Jade <jaderiver64@gmail.com>
2021-05-07 01:10:49 -0500

    client side coded

--- client/src/App.js
+++ client/src/App.js
@@ -21,18 +21,20 @@ const client = new ApolloClient({
 });
 
 function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={SearchBooks} />
+                        <Route exact path="/saved" component={SavedBooks} />
+                        <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+                    </Switch>
+                </>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;
