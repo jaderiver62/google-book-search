7e421dd9fd3092272ab0432510a9a6fd630df69c
Nina Jade <jaderiver64@gmail.com>
2021-05-07 11:00:13 -0500

    more bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
+
 
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
 
-import SearchBooks from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
-import Navbar from "./components/Navbar";
 
 const client = new ApolloClient({
     request: operation => {
@@ -23,31 +24,20 @@ const client = new ApolloClient({
 });
 
 function App() {
-    return ( <
-        ApolloProvider client = { client } >
-        <
-        Router >
-        <
-        >
-        <
-        Navbar / >
-        <
-        Switch >
-        <
-        Route exact path = "/"
-        component = { SearchBooks }
-        /> <
-        Route exact path = "/saved"
-        component = { SavedBooks }
-        /> <
-        Route render = {
-            () => < h1 className = "display-2" > Wrong page! < /h1>} / >
-            <
-            /Switch> <
-            /> <
-            /Router> <
-            /ApolloProvider>
-        );
-    }
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={SearchBooks} />
+                        <Route exact path="/saved" component={SavedBooks} />
+                        <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+                    </Switch>
+                </>
+            </Router>
+        </ApolloProvider>
+    );
+}
 
-    export default App;
+export default App;
