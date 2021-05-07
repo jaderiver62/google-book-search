511388d562a2577fefbb09e133b8ae26f7cba0b3
Nina Cummings <67253353+jaderiver62@users.noreply.github.com>
2021-05-07 18:18:45 -0500

    Update SearchBooks.js
    Tiny typo

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -31,7 +31,7 @@ const SearchBooks = () => {
             const response = await searchGoogleBooks(searchInput);
 
             if (!response.ok) {
-                throw new Error("something went wrong!");
+                throw new Error("Something went wrong!");
             }
 
             const { items } = await response.json();
